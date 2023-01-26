22cde84a494ce1c0dabc55ea5ca02ef6fb380a00
Rahimi Sahal <norahimisahal3@student.usm.my>
2023-01-26 15:27:11 +0800

    Mainframe
    improving gui

--- PhoneBook.java
+++ PhoneBook.java
@@ -348,161 +348,248 @@ class DeleteEventHandler implements ActionListener
 	}
 }
 
-class MainFrame extends JFrame
-{
-	JTextField srchField = new JTextField(15);
-	JButton srchBtn = new JButton("SEARCH");
-	
-	JButton addBtn = new JButton("ADD");
-	JRadioButton rbtn1 = new JRadioButton("General");
-	JRadioButton rbtn2 = new JRadioButton("University");
-	JRadioButton rbtn3 = new JRadioButton("Company");
-	ButtonGroup buttonGroup = new ButtonGroup();
-	
-	JLabel nameLabel = new JLabel("NAME");
-	JTextField nameField = new JTextField(15);
-	JLabel phoneLabel = new JLabel("PHONE NUMBER");
-	JTextField phoneField = new JTextField(15);
-	JLabel majorLabel = new JLabel("MAJOR");
-	JTextField majorField = new JTextField(15);
-	JLabel yearLabel = new JLabel("YEAR");
-	JTextField yearField = new JTextField(15);
-		
-	JTextField delField = new JTextField(15);
-	JButton delBtn = new JButton("DEL");
-	
-	JTextArea textArea = new JTextArea(10, 25);
-
-	public MainFrame(String title)
+	//Design and layout of GUI.
+	class MainFrame extends JFrame
 	{
-		super(title);
-		setBounds(100, 200, 330, 450);
-		setSize(730,350);
-		setLayout(new GridLayout(0,2,0,0));
-		Border border = BorderFactory.createEtchedBorder();
-		
-		Border srchBorder = BorderFactory.createTitledBorder(border, "Search");
-		JPanel srchPanel = new JPanel();
-		srchPanel.setBorder(srchBorder);
-		srchPanel.setLayout(new FlowLayout());
-		srchPanel.add(srchField);
-		srchPanel.add(srchBtn);
-		
-		Border addBorder=BorderFactory.createTitledBorder(border, "Add");
-		JPanel addPanel = new JPanel();
-		addPanel.setBorder(addBorder);
-		addPanel.setLayout(new FlowLayout());
-		
-		JPanel addInputPanel = new JPanel();
-		addInputPanel.setLayout(new GridLayout(0,2,5,5));
-		
-		buttonGroup.add(rbtn1);
-		buttonGroup.add(rbtn2);
-		buttonGroup.add(rbtn3);
-		
-		addPanel.add(rbtn1);
-		addPanel.add(rbtn2);
-		addPanel.add(rbtn3);
-		addPanel.add(addBtn);
-		
-		addInputPanel.add(nameLabel);
-		addInputPanel.add(nameField);
-		addInputPanel.add(phoneLabel);
-		addInputPanel.add(phoneField);
-		addInputPanel.add(majorLabel);
-		addInputPanel.add(majorField);
-		addInputPanel.add(yearLabel);
-		addInputPanel.add(yearField);
-		
-		majorLabel.setVisible(false);
-		majorField.setVisible(false);
-		yearLabel.setVisible(false);
-		yearField.setVisible(false);
-		
-		rbtn1.setSelected(true);
-		addPanel.add(addInputPanel);
-		
-		rbtn1.addItemListener(
-				new ItemListener()
-				{
-					public void itemStateChanged(ItemEvent e)
+		JTextField srchField = new JTextField(15);
+		JButton srchBtn = new JButton("SEARCH");
+
+		JButton addBtn = new JButton("ADD");
+		JRadioButton rbtn1 = new JRadioButton("General");
+		JRadioButton rbtn2 = new JRadioButton("University");
+		JRadioButton rbtn3 = new JRadioButton("Company");
+		JRadioButton rbtn4 = new JRadioButton("Male");
+		JRadioButton rbtn5 = new JRadioButton("Female");
+		ButtonGroup buttonGroup = new ButtonGroup();
+		ButtonGroup buttonGroup2 = new ButtonGroup();
+
+		JLabel nameLabel = new JLabel("NAME");
+		JTextField nameField = new JTextField(15);
+		JLabel phoneLabel = new JLabel("PHONE NUMBER");
+		JTextField phoneField = new JTextField(15);
+		JLabel noteLabel = new JLabel("NOTE");
+		JTextField noteField = new JTextField(15);
+		JLabel dateLabel = new JLabel("BD");
+		JTextField dateField = new JTextField(15);
+		JLabel majorLabel = new JLabel("MAJOR");
+		JTextField majorField = new JTextField(15);
+		JLabel yearLabel = new JLabel("YEAR");
+		JTextField yearField = new JTextField(15);
+		JTextField printField = new JTextField(15);
+		JButton printButton = new JButton("PRINT(.TXT)");
+		JTextField delField = new JTextField(15);
+		JButton delBtn = new JButton("DEL");
+
+		JTextArea textArea = new JTextArea(10, 25);
+
+		//Creating the GUI.
+		public MainFrame(String title)
+		{
+			super(title);
+			setBounds(100, 200, 330, 450);
+			setSize(1600,550);
+			setLayout(new GridLayout(0,2,0,0));
+			Border border = BorderFactory.createEtchedBorder();
+
+			Border srchBorder = BorderFactory.createTitledBorder(border, "Search");
+			JPanel srchPanel = new JPanel();
+			srchPanel.setBorder(srchBorder);
+			srchPanel.setLayout(new FlowLayout());
+			srchPanel.add(srchField);
+			srchPanel.add(srchBtn);
+
+			// customize color define
+			Color c1 = new Color(255,204,0);
+			Color c2 = new Color(255, 255, 204);
+
+			// customize search button background
+			srchPanel.setBackground(c1);
+			ImageIcon img = new ImageIcon(ClassLoader.getSystemResource("search.jpg"));
+			srchBtn.setIcon(img);
+			srchBtn.setPreferredSize(new Dimension(50,23));
+
+			Border printBorder = BorderFactory.createTitledBorder(border,"Print");
+			JPanel printPanel = new JPanel();
+			printPanel.setBorder(printBorder);
+			printPanel.setLayout(new FlowLayout());
+			printPanel.add(printField);
+			printPanel.add(printButton);
+			printPanel.setBackground(c1); // customize panel color background
+
+			Border addBorder=BorderFactory.createTitledBorder(border, "Add");
+			JPanel addPanel = new JPanel();
+			addPanel.setBorder(addBorder);
+			addPanel.setLayout(new FlowLayout());
+
+			// customize add button background
+			addPanel.setBackground(c1);
+			ImageIcon img2 = new ImageIcon(ClassLoader.getSystemResource("add.jpg"));
+			addBtn.setIcon(img2);
+			addBtn.setPreferredSize(new Dimension(50,23));
+
+			// customize font
+			nameLabel.setFont(new Font("Monospaced", Font.BOLD, 15));
+			noteLabel.setFont(new Font("Monospaced", Font.BOLD, 15));
+			majorLabel.setFont(new Font("Monospaced", Font.BOLD, 15));
+			yearLabel.setFont(new Font("Monospaced", Font.BOLD, 15));
+			phoneLabel.setFont(new Font("Monospaced", Font.BOLD, 15));
+			dateLabel.setFont(new Font("Monospaced", Font.BOLD, 15));
+			srchField.setFont(new Font("Monospaced", Font.BOLD, 15));
+			srchField.setPreferredSize(new Dimension(500,23));
+			nameField.setFont(new Font("Monospaced", Font.BOLD, 15));
+			noteField.setFont(new Font("Monospaced", Font.BOLD, 15));
+			majorField.setFont(new Font("Monospaced", Font.BOLD, 15));
+			yearField.setFont(new Font("Monospaced", Font.BOLD, 15));
+			phoneField.setFont(new Font("Monospaced", Font.BOLD, 15));
+			rbtn1.setFont(new Font("Arial", Font.BOLD, 16));
+			rbtn2.setFont(new Font("Arial", Font.BOLD, 16));
+			rbtn3.setFont(new Font("Arial", Font.BOLD, 16));
+			nameField.setBackground(c2);
+			srchField.setBackground(c2);
+			noteField.setBackground(c2);
+			majorField.setBackground(c2);
+			yearField.setBackground(c2);
+			phoneField.setBackground(c2);
+			delField.setBackground(c2);
+			printField.setBackground(c2);
+			dateField.setBackground(c2);
+
+			JPanel addInputPanel = new JPanel();
+			addInputPanel.setLayout(new GridLayout(0,2,5,5));
+
+			buttonGroup.add(rbtn1);
+			buttonGroup.add(rbtn2);
+			buttonGroup.add(rbtn3);
+
+			// customize button background
+			rbtn1.setBackground(c1);
+			rbtn2.setBackground(c1);
+			rbtn3.setBackground(c1);
+			addInputPanel.setBackground(c1);
+
+			addPanel.add(rbtn1);
+			addPanel.add(rbtn2);
+			addPanel.add(rbtn3);
+			addPanel.add(addBtn);
+
+			addInputPanel.add(nameLabel);
+			addInputPanel.add(nameField);
+			addInputPanel.add(phoneLabel);
+			addInputPanel.add(phoneField);
+			addInputPanel.add(noteLabel);
+			addInputPanel.add(noteField);
+			addInputPanel.add(dateLabel);
+			addInputPanel.add(dateField);
+
+			buttonGroup2.add(rbtn4);
+			buttonGroup2.add(rbtn5);
+
+			addInputPanel.add(rbtn4);
+			addInputPanel.add(rbtn5);
+
+			addInputPanel.add(majorLabel);
+			addInputPanel.add(majorField);
+			addInputPanel.add(yearLabel);
+			addInputPanel.add(yearField);
+
+			majorLabel.setVisible(false);
+			majorField.setVisible(false);
+			yearLabel.setVisible(false);
+			yearField.setVisible(false);
+
+			rbtn1.setSelected(true);
+			addPanel.add(addInputPanel);
+
+			rbtn1.addItemListener(
+					new ItemListener()
 					{
-						if(e.getStateChange() == ItemEvent.SELECTED)
+						//For PhoneInfo.
+						public void itemStateChanged(ItemEvent e)
 						{
-							majorLabel.setVisible(false);
-							majorField.setVisible(false);
-							yearLabel.setVisible(false);
-							yearField.setVisible(false);			
-							majorField.setText("");
-							yearField.setText("");
+							if(e.getStateChange() == ItemEvent.SELECTED)
+							{
+								majorLabel.setVisible(false);
+								majorField.setVisible(false);
+								yearLabel.setVisible(false);
+								yearField.setVisible(false);
+								majorField.setText("");
+								yearField.setText("");
+							}
 						}
 					}
-				}
-		);
-		
-		rbtn2.addItemListener(
-				new ItemListener()
-				{
-					public void itemStateChanged(ItemEvent e)
+			);
+
+			rbtn2.addItemListener(
+					new ItemListener()
 					{
-						if(e.getStateChange() == ItemEvent.SELECTED)
+						//For PhoneUnivInfo.
+						public void itemStateChanged(ItemEvent e)
 						{
-							majorLabel.setVisible(true);
-							majorLabel.setText("MAJOR");
-							majorField.setVisible(true);
-							yearLabel.setVisible(true);
-							yearField.setVisible(true);
+							if(e.getStateChange() == ItemEvent.SELECTED)
+							{
+								majorLabel.setVisible(true);
+								majorLabel.setText("MAJOR");
+								majorField.setVisible(true);
+								yearLabel.setVisible(true);
+								yearField.setVisible(true);
+							}
 						}
 					}
-				}
-		);
-		
-		rbtn3.addItemListener(
-				new ItemListener()
-				{
-					public void itemStateChanged(ItemEvent e)
+			);
+
+			rbtn3.addItemListener(
+					new ItemListener()
 					{
-						if(e.getStateChange() == ItemEvent.SELECTED)
+						//For PhoneCompanyInfo.
+						public void itemStateChanged(ItemEvent e)
 						{
-							majorLabel.setVisible(true);
-							majorLabel.setText("COMPANY");
-							majorField.setVisible(true);
-							yearLabel.setVisible(false);
-							yearField.setVisible(false);
-							yearField.setText("");
+							if(e.getStateChange() == ItemEvent.SELECTED)
+							{
+								majorLabel.setVisible(true);
+								majorLabel.setText("COMPANY");
+								majorField.setVisible(true);
+								yearLabel.setVisible(false);
+								yearField.setVisible(false);
+								yearField.setText("");
+							}
 						}
 					}
-				}
-		);
-		
-		Border delBorder = BorderFactory.createTitledBorder(border, "Delete");
-		JPanel delPanel = new JPanel();
-		delPanel.setBorder(delBorder);
-		delPanel.setLayout(new FlowLayout());
-		delPanel.add(delField);
-		delPanel.add(delBtn);
-		
-		JScrollPane scrollTextArea = new JScrollPane(textArea);	
-		Border textBorder=BorderFactory.createTitledBorder(border, "Infomation Board");
-		scrollTextArea.setBorder(textBorder);
-		
-		JPanel actionPanel = new JPanel();
-		actionPanel.setLayout(new BorderLayout());
-		actionPanel.add(srchPanel, BorderLayout.NORTH);
-		actionPanel.add(addPanel, BorderLayout.CENTER);
-		actionPanel.add(delPanel, BorderLayout.SOUTH);
-		
-		add(actionPanel);
-		add(scrollTextArea);
-		
-		srchBtn.addActionListener(new SearchEventHandler(srchField, textArea));
-		addBtn.addActionListener(new AddEventHandler(nameField, phoneField, majorField, yearField, textArea));
-		delBtn.addActionListener(new DeleteEventHandler(delField, textArea));
-		
-		setVisible(true);
-		setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);	
+			);
+
+			Border delBorder = BorderFactory.createTitledBorder(border, "Delete");
+			JPanel delPanel = new JPanel();
+			delPanel.setBorder(delBorder);
+			delPanel.setLayout(new FlowLayout());
+			delPanel.add(delField);
+			delPanel.add(delBtn);
+			delPanel.setBackground(c1); // customize delete panel background
+
+			JScrollPane scrollTextArea = new JScrollPane(textArea);
+			Border textBorder=BorderFactory.createTitledBorder(border, "Information Board");
+			scrollTextArea.setBorder(textBorder);
+			// customize information panel background
+			scrollTextArea.setBackground(c1);
+			textArea.setBackground(c2);
+
+			JPanel actionPanel = new JPanel();
+			actionPanel.setLayout(new BorderLayout());
+			actionPanel.add(srchPanel, BorderLayout.NORTH);
+			actionPanel.add(printPanel, BorderLayout.BEFORE_LINE_BEGINS);
+			actionPanel.add(addPanel, BorderLayout.CENTER);
+			actionPanel.add(delPanel, BorderLayout.SOUTH);
+
+			add(actionPanel);
+			add(scrollTextArea);
+
+			srchBtn.addActionListener(new SearchEventHandler(srchField, textArea));
+			addBtn.addActionListener(new AddEventHandler(nameField, phoneField, noteField, dateField, rbtn4, majorField, yearField, textArea));
+			printButton.addActionListener(new PrintEventHandler(printField, textArea));
+			delBtn.addActionListener(new DeleteEventHandler(delField, textArea));
+
+			setVisible(true);
+			setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+		}
 	}
-}
 
 //Setting up the GUI.
 class PhoneBook extends JFrame implements ActionListener {
